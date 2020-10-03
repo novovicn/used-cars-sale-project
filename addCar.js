aea6b1482b8b5d285d45a2a17301d577b05f467c
Nenad Novovic <novovicnenad96@gmail.com>
2020-10-04 00:06:29 +0200

    user must be logged in to add a car, only an user who added a car can delete it, other users can buy it, conditional rendering

--- addCar.js
+++ addCar.js
@@ -1,6 +1,46 @@
 
 document.getElementById('submit').addEventListener('click', addCartoLS);
 
+const loggedUser = JSON.parse(sessionStorage.getItem('loggedUser'));
+
+
+
+window.onload = function(){
+
+    /*
+     THERE MUST BE A BETTER way of doing this*---the problem : first if is when 
+     ss is EMPTY, another if is when ss is just an empty object ( should I change the logout logic??)
+     */
+
+        if(loggedUser != null){
+
+            if (loggedUser.username != null){
+                document.getElementById('add__car--form').style.display='block';
+                document.getElementById('please__login').style.display= 'none';
+                
+        
+            }else{
+                document.getElementById('add__car--form').style.display="none";
+                document.getElementById('please__login').innerHTML = "Please <a href='./index.html'>log in</a> to add your car";
+            }
+        }else{
+            document.getElementById('add__car--form').style.display="none";
+                document.getElementById('please__login').innerHTML = "Please <a href='./index.html'>log in</a> to add your car";
+        }
+
+    
+       
+    }
+
+
+// TODO: Moras biti ulogovan da bi uopste dosao na ovu stranicu -- DONE
+
+// TODO: Uhvatiti koji korisnik je trenutno u SS i na osnovu toga to ubaciti u polje VLASNIK vozila --DONE
+// , i onda posle kad se klikne na obrisi oglas, to moze da se desi samo ako u tom trenutku u SS username je isto kao i vlasnik vozila
+
+
+// tesko ce ici ovo sa ImageURL jer ce gugl verovatno zabraniti, tako da cu morati ako nema imageURL da izbacim 
+// neku sliku koja je u mom folderu, ili da ukinem to nego da sam sa vikipedie vuce tu sliku 
 
 function addCartoLS(){
 
@@ -11,7 +51,7 @@ function addCartoLS(){
     const mileage = document.getElementById('mileage').value;
 
     if(imageURL == ""){
-        imageURL = "https://lh3.googleusercontent.com/proxy/oly9gbPaLge9AROjT2BL0OGv5ytkxgkOkH-1iqg7e-Xibmz-GfSvkNdSGEHJMJvjBAeyM9-SAXljql0Q7aA_nrc-3boVfbH3sYjUYKIz3eSVfTWBFlER_mhQ_KPqww"
+        imageURL = "https://www.auto123.com/static/auto123/images/unknown.692d9ec5c563.png"
     }
 
 
@@ -22,7 +62,8 @@ function addCartoLS(){
         model: model, 
         year: year, 
         imageURL: imageURL,
-        mileage: mileage
+        mileage: mileage,
+        owner: loggedUser.username
     }
 
     let carList = retrieveCarsLS();
--- carlist.js
+++ carlist.js
@@ -4,12 +4,66 @@ const container = document.getElementsByClassName('container')[0];
 
 const carlist = JSON.parse(localStorage.getItem('cars'));
 
-
+if(sessionStorage.getItem('loggedUser') != null){
+    var currentUser = JSON.parse(sessionStorage.getItem('loggedUser')).username;
+}
 
 if(carlist !== null){
     for ( let i = 0; i < carlist.length; i++){
-        console.log(carlist[i]);
+
+        // CONDITIONAL RENDERING - owner can delete the post, while other users can buy it
+        if(carlist[i].owner == currentUser){
+            var main_button = "<button class='delete__car'>Delete car </button>"
+        }else{
+            var main_button = "<button class='buy__car'>Buy this car </button>"
+        }
+
+        let car = document.createElement('div');
+        car.innerHTML = `
+        <div class="car__card">
+        <img class="carlist__image" src="${carlist[i].imageURL}"/>
+        <h1>${carlist[i].brand}</h1>
+        <h2>${carlist[i].model}</h2>
+        <h3>${carlist[i].year}</h3>
+        <h4>Owner:${carlist[i].owner}</h4>
+        <button class="see__more">More info</button>
+        ${main_button}
+        </div>
+        `
+        container.appendChild(car);
     }
 }else{
-    console.log('No cars at the moment');
+    container.innerHTML= "There aren't any cars for sale at the moment!";
 }
+
+
+// ne znam zasto ne izbacuje gresku ispod kada nema nijednog dugmeta sa klasom buy__car onda kada je sve prazno..
+
+const buy__buttons = document.getElementsByClassName('buy__car');
+
+
+for( let i = 0; i < buy__buttons.length; i++){
+    buy__buttons[i].addEventListener('click', addItemToCart);
+}
+
+const delete__buttons = document.getElementsByClassName('delete__car');
+
+for(let i = 0; i < delete__buttons.length; i++){
+    delete__buttons[i].addEventListener('click', deleteItem);
+}
+
+
+
+
+function addItemToCart(){
+    alert('Item added to cart');
+
+    // code here to add item to cart...
+}
+
+function deleteItem(){
+    alert('are you sure you want to delete this item?');
+
+    // code here to delete item...
+}
+// OVDE BI trebala neka promisa da padne jer nece uvek biti auta na sajtu........
