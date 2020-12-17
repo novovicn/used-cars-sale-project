7791c17c82342ab700aa014b6c880b1800cb2351
Nenad Novovic <novovicnenad96@gmail.com>
2020-12-17 13:47:32 +0100

    cars stored in firestore instead of LS - trial version, needs code refactoring

--- addCar.js
+++ addCar.js
@@ -1,133 +1,157 @@
-
-const carForm = document.getElementById('add-car-form');
-const loginPrompt = document.getElementById('login-prompt');
-
-carForm.onsubmit = addCartoLS;
-const loggedUser = JSON.parse(sessionStorage.getItem('loggedUser'));
-const carlist = retrieveCarsLS();
-
-const carFromHeading = document.getElementsByClassName('add-car-heading')[0];
-const carAddedAlert = document.getElementsByClassName('car-added')[0];
-
-
+const carForm = document.getElementById("add-car-form");
+const loginPrompt = document.getElementById("login-prompt");
+const loggedUser = JSON.parse(sessionStorage.getItem("loggedUser"));
+const carFromHeading = document.getElementsByClassName("add-car-heading")[0];
+const carAddedAlert = document.getElementsByClassName("car-added")[0];
 const errorMessages = document.getElementById("car-form-errors");
+const hamburgerBtnSellCar = document.getElementsByClassName("hamburger-btn")[0];
 
-
-const hamburgerBtnSellCar = document.getElementsByClassName('hamburger-btn')[0];
-
-
-
-console.log(errorMessages);
-
-
-window.onload = function(){
-    /*
-     THERE MUST BE A BETTER way of doing this*---the problem : first if is when 
-     ss is EMPTY, another if is when ss is just an empty object ( should I change the logout logic??)
-     */
-
-        if(loggedUser != null){
-            if (loggedUser.username != null){
-                carForm.style.display='block';
-                loginPrompt.style.display= 'none';
-                
-            }else{
-                carForm.style.display="none";
-                loginPrompt.style.display="inherit";
-                carFromHeading.style.display = 'none';
-            }
-        }else{
-                carForm.style.display="none";
-                loginPrompt.style.display="inherit";
-                carFromHeading.style.display = 'none';
-        }
-    }
-
-function addCartoLS(e){
-
-    
-
-    errorMessages.innerHTML = '';
-
-    e.preventDefault();
-
-    const brand = document.getElementById('brand').value;
-    const model = document.getElementById('model').value;
-    const year = document.getElementById('year').value;
-    let imageURL = document.getElementById('image-url').value;
-    const mileage = document.getElementById('mileage').value;
-    const vin = document.getElementById('vin').value;
-    const price = document.getElementById('price').value;
-
-
-    if(imageURL == ""){
-        imageURL = "img/unknown.png"
-    }
-
-    // INPUT VALIDATION 
-
-    // must populate required fields
-    if( brand == "" || model == "" || year == "" || vin == "" || price == ""){
-        errorMessages.innerHTML +='<p>Please populate required fields</p>';
-        return;
-    }
-
-    //year 
-
-    if(year < 1900){
-        errorMessages.innerHTML += '<p>Hmm..too old car for this brand new site!</p>';
-        return;
-    }
-
-    if(year > 2021){
-        errorMessages.innerHTML += '<p>Hmm..you are selling the car that isn\'t made yet!</p>';
-        return;
-    }
-
-    // no two same vins
-    if(carlist){
-        for( let i = 0; i< carlist.length; i++){
-            if(carlist[i].VIN === vin){
-                errorMessages.innerHTML += '<p> Can\'t add two cars with the same VIN</p>';
-                return;
-            }
-        }
-    
-    };
-
-    // NECE DA GA SKLONI PRIJAVI GRESKU ALI NE IZADJE NEGO GA IPAK POSALJE U LS !!!!
-   
-    let cars=[];
-
-    let car = {
-        brand: brand,
-        model: model, 
-        year: year, 
-        imageURL: imageURL,
-        mileage: mileage,
-        owner: loggedUser.username,
-        VIN: vin,
-        price: price,
-        sold: false,
-        created_at: Date.now()
-    }
-
-    if(carlist != null){
-        carlist.push(car);
-        var carsLS = JSON.stringify(carlist);
-    }else{
-        cars.push(car);
-        var carsLS = JSON.stringify(cars);
-    }
-    localStorage.setItem('cars', carsLS);   
-
-    carForm.classList.add('vanish');
-    carFromHeading.classList.add('vanish');
-    carAddedAlert.classList.add('appear');
-
+function retrieveCarsDB() {
+  return new Promise((res, rej) => {
+    axios
+      .get("https://used-cars-sale-default-rtdb.firebaseio.com/cars.json")
+      .then((response) => {
+        let cars = Object.values(response.data);
+        console.log(cars);
+        res(cars);
+      })
+      .catch((err) => console.log(err));
+  });
 }
 
-function retrieveCarsLS() {
-    let carsLS = JSON.parse(localStorage.getItem('cars'));
-    return carsLS;
+function retrieveCarsFirestore() {
+  return new Promise((resolve, reject) => {
+    db.collection("cars")
+      .get()
+      .then((snapshot) => {
+        let carsArray = [];
+        console.log(snapshot.docs);
+        snapshot.docs.forEach((doc) => carsArray.push(doc.data()));
+        console.log(carsArray);
+        resolve(carsArray);
+      })
+      .catch((err) => console.log(err));
+  });
 }
+
+// const cars = db.collection('cars').get().then(snapshot => {
+//   let carsArray = [];
+//   console.log(snapshot.docs);
+//   snapshot.docs.forEach(doc => carsArray.push(doc.data()));
+//   console.log(carsArray);
+//   return carsArray;
+// });
+
+carForm.onsubmit = addCarToDB;
+
+// TODO: IZ PROMISE IZVUCI CARLIST NEKAKO......
+
+window.onload = function () {
+  if (loggedUser != null) {
+    if (loggedUser.username != null) {
+      carForm.style.display = "block";
+      loginPrompt.style.display = "none";
+    } else {
+      carForm.style.display = "none";
+      loginPrompt.style.display = "inherit";
+      carFromHeading.style.display = "none";
+    }
+  } else {
+    carForm.style.display = "none";
+    loginPrompt.style.display = "inherit";
+    carFromHeading.style.display = "none";
+  }
+};
+
+async function addCarToDB(e) {
+  errorMessages.innerHTML = "";
+
+  e.preventDefault();
+
+  const brand = document.getElementById("brand").value;
+  const model = document.getElementById("model").value;
+  const year = document.getElementById("year").value;
+  let imageURL = document.getElementById("image-url").value;
+  const mileage = document.getElementById("mileage").value;
+  const vin = document.getElementById("vin").value;
+  const price = document.getElementById("price").value;
+
+  if (imageURL == "") {
+    imageURL = "img/unknown.png";
+  }
+
+  // INPUT VALIDATION
+
+  // must populate required fields
+  if (brand == "" || model == "" || year == "" || vin == "" || price == "") {
+    errorMessages.innerHTML += "<p>Please populate required fields</p>";
+    return;
+  }
+
+  //year
+
+  if (year < 1900) {
+    errorMessages.innerHTML +=
+      "<p>Hmm..too old car for this brand new site!</p>";
+    return;
+  }
+
+  if (year > 2021) {
+    errorMessages.innerHTML +=
+      "<p>Hmm..you are selling the car that isn't made yet!</p>";
+    return;
+  }
+
+  // no two same vins
+
+  let carlist = await retrieveCarsFirestore().then((res) => {
+    return res;
+  });
+
+  console.log(carlist);
+
+  if (carlist) {
+    for (let i = 0; i < carlist.length; i++) {
+      if (carlist[i].VIN === vin) {
+        errorMessages.innerHTML +=
+          "<p> Can't add two cars with the same VIN</p>";
+        return;
+      }
+    }
+  }
+
+  // let cars=[];
+
+  let car = {
+    brand: brand,
+    model: model,
+    year: year,
+    imageURL: imageURL,
+    mileage: mileage,
+    owner: loggedUser.username,
+    VIN: vin,
+    price: price,
+    sold: false,
+    created_at: Date.now(),
+  };
+
+  //////////// PROBA DODAVANJE U DATABASE
+
+  db.collection("cars").add(car);
+
+  // if(carlist != null){
+  //     carlist.push(car);
+  //     var carsLS = JSON.stringify(carlist);
+  // }else{
+  //     cars.push(car);
+  //     var carsLS = JSON.stringify(cars);
+  // }
+  // localStorage.setItem('cars', carsLS);
+
+  carForm.classList.add("vanish");
+  carFromHeading.classList.add("vanish");
+  carAddedAlert.classList.add("appear");
+}
+
+// let carsLS = JSON.parse(localStorage.getItem('cars'));
+// return carsLS;
--- carlist.js
+++ carlist.js
@@ -1,65 +1,78 @@
-
-/*
-AKO neulogovan gledas carlist, ne treba nijedno dugme da se prikazuje, samo da pise LOG IN to buy a car -d o n e
-
-*/ 
-
-const container = document.getElementsByClassName('sale-container')[0];
-const carlist = JSON.parse(localStorage.getItem('cars'));
-const buyCheckout = document.getElementById('buy-checkout');
-const moreInfoSection = document.getElementsByClassName('more-info-section')[0];
-const carlistHeading = document.getElementsByClassName('carlist-heading')[0];
-
-
-
-
+const container = document.getElementsByClassName("sale-container")[0];
+const buyCheckout = document.getElementById("buy-checkout");
+const moreInfoSection = document.getElementsByClassName("more-info-section")[0];
+const carlistHeading = document.getElementsByClassName("carlist-heading")[0];
 
 window.onload = () => {
-    buyCheckout.style.display = 'none';
-    moreInfoSection.style.display = 'none';
-    renderElements(carlist);
+  buyCheckout.style.display = "none";
+  moreInfoSection.style.display = "none";
+//   renderElements(carlist);
+
+  db.collection("cars")
+    .get()
+    .then((snapshot) => {
+      let carsArray = [];
+      snapshot.docs.forEach((doc) =>
+        carsArray.push({ ...doc.data(), id: doc.id })
+      );
+      renderElements(carsArray);
+    });
+};
+
+function retrieveCarsFromDatabase() {
+  return new Promise((resolve, reject) => {
+    db.collection("cars")
+      .get()
+      .then((snapshot) => {
+        let carsArray = [];
+        console.log(snapshot.docs);
+        snapshot.docs.forEach((doc) => carsArray.push(doc.data()));
+        console.log(carsArray);
+        resolve(carsArray);
+      })
+      .catch((err) => console.log(err));
+  });
 }
 
-if(sessionStorage.getItem('loggedUser') != null){
-    var currentUser = JSON.parse(sessionStorage.getItem('loggedUser')).username;
+if (sessionStorage.getItem("loggedUser") != null) {
+  var currentUser = JSON.parse(sessionStorage.getItem("loggedUser")).username;
 }
 
-async function renderElements(elements){
+async function renderElements(elements) {
+  container.innerHTML = "";
 
-    container.innerHTML = '';
-    
-    if(elements != null){
-
-        // newest first
-        for(let i = 0; i < elements.length -1; i++){
-            for( let j = i+1; j < elements.length; j++){
-                if(elements[i].created_at < elements[j].created_at){
-                    let temp = elements[i];
-                    elements[i] = elements[j];
-                    elements[j] = temp;
-                }
-            }
+  if (elements != null) {
+    // newest first
+    for (let i = 0; i < elements.length - 1; i++) {
+      for (let j = i + 1; j < elements.length; j++) {
+        if (elements[i].created_at < elements[j].created_at) {
+          let temp = elements[i];
+          elements[i] = elements[j];
+          elements[j] = temp;
         }
+      }
+    }
 
-        for ( let i = 0; i < elements.length; i++){
-            // console.log(elements[i]);
-            // CONDITIONAL RENDERING - owner can delete the post, while other users can buy it
-            if(elements[i].owner == currentUser){
-                var mainButton = "<button class='main-button delete-car'>Delete</button>"
-            }else{
-                var mainButton = "<button class='main-button buy-car'>Buy</button>"
-            }
-            if(elements[i].sold === true){
-                image_url = "https://www.benchmarkrealty.co.nz/wp-content/uploads/2018/06/sold-stamp-3.png";
-                if(elements[i].owner !== currentUser){
-                    var mainButton = ''; // can't be bought anymore, but stll can be deleted by owner!
-                }    
-            }else{
-                image_url = elements[i].imageURL;
-            }
-            let car = document.createElement('div');
-            car.innerHTML = `
-            <div class="car-card">
+    for (let i = 0; i < elements.length; i++) {
+      // CONDITIONAL RENDERING - owner can delete the post, while other users can buy it
+      if (elements[i].owner == currentUser) {
+        var mainButton =
+          "<button class='main-button delete-car'>Delete</button>";
+      } else {
+        var mainButton = "<button class='main-button buy-car'>Buy</button>";
+      }
+      if (elements[i].sold === true) {
+        image_url =
+          "https://www.benchmarkrealty.co.nz/wp-content/uploads/2018/06/sold-stamp-3.png";
+        if (elements[i].owner !== currentUser) {
+          var mainButton = ""; // can't be bought anymore, but stll can be deleted by owner!
+        }
+      } else {
+        image_url = elements[i].imageURL;
+      }
+      let car = document.createElement("div");
+      car.innerHTML = `
+            <div class="car-card" data-id="${elements[i].id}">
                 <img class="carlist-image" src="${image_url}"/>
                 <div class="carlist-about">
                     <h2 class='carlist-brand-and-model'>${elements[i].brand}  ${elements[i].model}</h2>
@@ -71,158 +84,145 @@ async function renderElements(elements){
                     ${mainButton}
                 </div>
             </div>
-            `
-            container.appendChild(car);
-            // console.log('Uradjeno!');
-        }
-    }else{
-        container.innerHTML= "<h1 class='no-cars'>There aren't any cars for sale at the moment!</h1>";
+            `;
+      container.appendChild(car);
     }
-    await addListeners();
-        
+  } else {
+    container.innerHTML =
+      "<h1 class='no-cars'>There aren't any cars for sale at the moment!</h1>";
+  }
+  await addListeners();
 }
 
-function addListeners(){
-    let buyButtons = document.getElementsByClassName('buy-car');
-    let deleteButtons = document.getElementsByClassName('delete-car');
-    let moreInfoButtons = document.getElementsByClassName('see-more');
+function addListeners() {
+  let buyButtons = document.getElementsByClassName("buy-car");
+  let deleteButtons = document.getElementsByClassName("delete-car");
+  let moreInfoButtons = document.getElementsByClassName("see-more");
 
-    for(let i = 0; i < buyButtons.length; i++){
-        buyButtons[i].addEventListener('click', addItemToCart);
-    }
+  [...buyButtons].forEach(button => {
+      button.addEventListener('click', addItemToCart);
+  });
 
-    for(let i = 0; i < deleteButtons.length; i++){
-        deleteButtons[i].addEventListener('click', deleteItem);
-    }
+  [...deleteButtons].forEach(button => {
+    button.addEventListener('click', deleteItem);
+  });
 
-    for(let i = 0; i < moreInfoButtons.length; i++){
-        moreInfoButtons[i].addEventListener('click', moreInfo);
-    }
+  [...moreInfoButtons].forEach(button => {
+    button.addEventListener('click', moreInfo);
+  });
 
 }
 
-function addItemToCart(e){
+function addItemToCart(e) {
 
-    console.log(e);
-    if(currentUser){
-        document.getElementById('carlist').style.display = 'none';
-    carlistHeading.style.display = 'none';
+  if (currentUser) {
+    document.getElementById("carlist").style.display = "none";
+    carlistHeading.style.display = "none";
 
-    buyCheckout.style.display='block';
+    buyCheckout.style.display = "block";
 
     const itemToBuy = e.target.parentNode.parentNode;
 
-    let imageURL = itemToBuy.getElementsByClassName('carlist-image')[0].src;
-    let modelInfo = itemToBuy.getElementsByClassName('carlist-brand-and-model')[0].textContent;
-    // let year = itemToBuy.getElementsByClassName('carlist-year')[0].textContent;
-    // let owner = itemToBuy.getElementsByClassName('carlist__owner')[0].textContent;
-    let vin = itemToBuy.getElementsByClassName('carlist-vin')[0].textContent;
-    let price = itemToBuy.getElementsByClassName('carlist-price')[0].textContent;
+    console.log(itemToBuy);
 
-    let vinDiv = document.createElement('div');
-    vinDiv.setAttribute('id', 'vin-num');
-    vinDiv.style.display = 'none';
-    vinDiv.innerHTML = `${vin}`;
+    let id = itemToBuy.getAttribute("data-id");
+    console.log(id);
 
-    let cart = document.getElementById('buy-checkout-table');
+    let imageURL = itemToBuy.getElementsByClassName("carlist-image")[0].src;
+    let modelInfo = itemToBuy.getElementsByClassName(
+      "carlist-brand-and-model"
+    )[0].textContent;
+    // let vin = itemToBuy.getElementsByClassName('carlist-vin')[0].textContent;
+    let price = itemToBuy.getElementsByClassName("carlist-price")[0]
+      .textContent;
 
-    cart.style.display = 'inherit';
-    
-    let cart_row = document.createElement('div');
-    cart_row.setAttribute('class', 'second-row');
+    let vinDiv = document.createElement("div");
+    vinDiv.setAttribute("id", "vin-num");
+    vinDiv.style.display = "none";
+    vinDiv.innerHTML = `${id}`;
 
-    cart_row.innerHTML = 
-    `
+    let cart = document.getElementById("buy-checkout-table");
+
+    cart.style.display = "inherit";
+
+    let cart_row = document.createElement("div");
+    cart_row.setAttribute("class", "second-row");
+
+    cart_row.innerHTML = `
         <div class="buy-checkout-image" ><img src="${imageURL}"></img></div>
         <div class="buy-checkout-model">${modelInfo}</div>
         <div class="buy-checkout-price">€ ${price}</div>
-    `
+    `;
     cart.appendChild(cart_row);
     cart.appendChild(vinDiv);
-
-    }else{
-        alert('Please log in first!');
-    }
-
-    
+  } else {
+    alert("Please log in first!");
+  }
 }
 
-function buyACar(e){
+async function buyACar(e) {
+  console.log(e.target.parentNode.parentNode);
+  let id = document.getElementById("vin-num").textContent;
 
-    console.log(e.target.parentNode.children[1]);
-    let VIN = document.getElementById('vin-num').textContent;
-     //getting VIN number
+  await db.collection("cars").doc(id).update({ sold: true });
 
-    let carsFromLS = JSON.parse(localStorage.getItem('cars'));
+  alert("car successfully bought!");
 
-    carsFromLS.forEach( car => {
-        if( car.VIN === VIN ){
-            car.sold = true;
-        }
-    });
+  document.getElementById("carlist").style.display = "inherit";
+  carlistHeading.style.display = "inherit";
+  buyCheckout.style.display = "none";
+  window.location.reload();
+}
 
-    alert('car successfully bought!');
-    
-    document.getElementById('carlist').style.display = 'inherit';
-    carlistHeading.style.display = 'inherit';
-    buyCheckout.style.display='none';
+async function deleteItem(e) {
+  let id = e.target.parentNode.parentNode.getAttribute("data-id"); //mozda postoji laksi nacin da se do ovoga dodje
+
+  console.log(id);
+  // let storedCars = JSON.parse(localStorage.getItem('cars'));
+
+  let storedCars = await retrieveCarsFromDatabase();
+
+  let ask = window.confirm(`Are you sure you want to delete?`);
+  if (ask === false) {
+    return false;
+  } else {
+    const data = await db
+      .collection("cars")
+      .doc(id)
+      .delete()
+      .then(() => console.log("done"));
+
     window.location.reload();
-
-
-    localStorage.setItem('cars', JSON.stringify(carsFromLS));
-
+  }
 }
 
-function deleteItem(e){
+async function moreInfo(e) {
+  buyCheckout.style.display = "none";
+  container.style.display = "none";
+  moreInfoSection.style.display = "inherit";
+  carlistHeading.style.display = "none";
 
+  const carDetails = document.getElementsByClassName("car-details")[0];
 
-    let deleteVin = e.target.parentNode.parentNode.children[1].children[2].textContent; //mozda postoji laksi nacin da se do ovoga dodje
+  const moreInfoWiki = document.getElementsByClassName("more-info-wiki")[0];
 
-    let storedCars = JSON.parse(localStorage.getItem('cars'));
+  document
+    .getElementsByClassName("more-info-cancel")[0]
+    .addEventListener("click", funcX);
 
-    let ask = window.confirm(
-        `Are you sure you want to delete?`
-      );
-      if (ask === false) {
-        return false;
-      } else {
-        storedCars.forEach(( car, index ) => {
-            if(car.VIN == deleteVin){
-                storedCars.splice( index, 1 );
-            }
-        });
-    
-        localStorage.setItem('cars', JSON.stringify(storedCars));
-        window.location.reload();
-    
-    }
+  const carlist = await retrieveCarsFromDatabase();
 
-}
+  const carModelInfo =
+    e.target.parentNode.parentNode.children[1].children[0].textContent;
 
+  let moreInfoVin =
+    e.target.parentNode.parentNode.children[1].children[2].textContent;
 
-function moreInfo(e){
+  for (let i = 0; i < carlist.length; i++) {
+    if (carlist[i].VIN == moreInfoVin) {
+      // console.log(carlist[i]);
 
-    buyCheckout.style.display="none";
-    container.style.display = 'none';
-    moreInfoSection.style.display = 'inherit';
-    carlistHeading.style.display = 'none';
-
-    const carDetails = document.getElementsByClassName('car-details')[0];
-
-    const moreInfoWiki = document.getElementsByClassName('more-info-wiki')[0];
-
-    document.getElementsByClassName('more-info-cancel')[0].addEventListener('click', funcX)
-
-
-    const carModelInfo = e.target.parentNode.parentNode.children[1].children[0].textContent;
-  
-    let moreInfoVin = e.target.parentNode.parentNode.children[1].children[2].textContent;
-
-    for( let i = 0; i< carlist.length; i++){
-        if(carlist[i].VIN == moreInfoVin){
-            // console.log(carlist[i]);
-
-            carDetails.innerHTML = `
+      carDetails.innerHTML = `
                 <div class="car-details-image">
                     <img src="${carlist[i].imageURL}" alt="${carlist[i].brand}${carlist[i].model}" />
                 </div>
@@ -237,101 +237,109 @@ function moreInfo(e){
                     <p><span class='bold'>VIN:</span> ${carlist[i].VIN}</p>
                     <h1 class="car-details-big" ><span class='bold'>Price:</span> ${carlist[i].price} €</h1>
                 </div>
-            `
-
-        }
+            `;
     }
+  }
 
+  // console.log(moreInfoVin);
 
-    // console.log(moreInfoVin);
+  let wikiLink = `https://en.wikipedia.org/w/api.php?action=query&titles=${carModelInfo}&prop=extracts|pageimages|info&pithumbsize=400&inprop=url&redirects=&format=json&origin=*`;
 
-    let wikiLink = `https://en.wikipedia.org/w/api.php?action=query&titles=${carModelInfo}&prop=extracts|pageimages|info&pithumbsize=400&inprop=url&redirects=&format=json&origin=*`;
+  fetch(wikiLink)
+    .then((response) => response.json())
+    .then((data) => {
+      const pages = data.query.pages;
+      const article = Object.values(pages)[0];
+      const imgSrc = article.thumbnail.source;
 
-    fetch(wikiLink)
-    .then(response => response.json())
-    .then(data => {
-        const pages = data.query.pages;
-        const article = Object.values(pages)[0];
-        const imgSrc = article.thumbnail.source;
- 
-        document.getElementById('more-info-spinner').style.display='none';
+      document.getElementById("more-info-spinner").style.display = "none";
 
-        moreInfoWiki.innerHTML = `
+      moreInfoWiki.innerHTML = `
         <div class="wiki-image">
         <img  src="${imgSrc}"/>
         </div>
         <br/>
         <div class="wiki-textbox">
-        <p class="wiki-text"> ${article.extract.substring(0,1000)}...<a class="read-more" href=${article.fullurl} target="_blank">[read more on wikipedia]</a></p>
+        <p class="wiki-text"> ${article.extract.substring(
+          0,
+          1000
+        )}...<a class="read-more" href=${
+        article.fullurl
+      } target="_blank">[read more on wikipedia]</a></p>
         </div>
         `;
-    }).catch(err => {
-        document.getElementById('more-info-spinner').style.display='none';
-        moreInfoWiki.innerHTML = "<h1 class='car-details-big'>Sorry, no wikipedia info for such car.</h1>";
     })
-
-    
+    .catch((err) => {
+      document.getElementById("more-info-spinner").style.display = "none";
+      moreInfoWiki.innerHTML =
+        "<h1 class='car-details-big'>Sorry, no wikipedia info for such car.</h1>";
+    });
 }
 
 // exit from more info...
-function funcX(){
-    window.location.reload();
+function funcX() {
+  window.location.reload();
 }
 
-
-const ApproveBuyBtns = document.getElementsByClassName('buy-approve');
-for( let i = 0; i < ApproveBuyBtns.length; i++ ){
-    ApproveBuyBtns[i].addEventListener('click', buyACar);
+const ApproveBuyBtns = document.getElementsByClassName("buy-approve");
+for (let i = 0; i < ApproveBuyBtns.length; i++) {
+  ApproveBuyBtns[i].addEventListener("click", buyACar);
 }
 
+// FILTERING
 
+const searchByName = document.getElementById("search-brand");
+searchByName.addEventListener("input", filterCarsByName);
 
-// FILTERING 
+async function filterCarsByName() {
+  let carlist = await retrieveCarsFromDatabase();
 
-const searchByName = document.getElementById('search-brand')
-searchByName.addEventListener('input', filterCarsByName);
-
-function filterCarsByName(){
-
-    let filteredCars = carlist.filter( car => {
-        if((car.brand.toUpperCase()).indexOf(searchByName.value.trim().toUpperCase()) != -1 
-        || (car.model.toUpperCase()).indexOf(searchByName.value.trim().toUpperCase()) != -1){
-            return car;
-        }
-    });
-    
-
-    renderElements(filteredCars);
-
-}
-
-const btnSearch = document.getElementById('search-price');
-btnSearch.addEventListener('click', filterByPrice);
-
-function filterByPrice(){
-    let lowestPrice = document.getElementById('search-price-lowest').value;
-    let highestPrice = document.getElementById('search-price-highest').value;
-
-    // RADI ALI NE BI BILO LOSE UBACITI I SORTIRANJE PO CENI ASC I DESC...
-
-    if(highestPrice === ""){
-        highestPrice = 99999999999;
+  let filteredCars = carlist.filter((car) => {
+    if (
+      car.brand
+        .toUpperCase()
+        .indexOf(searchByName.value.trim().toUpperCase()) != -1 ||
+      car.model
+        .toUpperCase()
+        .indexOf(searchByName.value.trim().toUpperCase()) != -1
+    ) {
+      return car;
     }
+  });
 
-    if(lowestPrice === ""){
-        lowestPrice = 0;
+  renderElements(filteredCars);
+}
+
+const btnSearch = document.getElementById("search-price");
+btnSearch.addEventListener("click", filterByPrice);
+
+async function filterByPrice() {
+  let lowestPrice = document.getElementById("search-price-lowest").value;
+  let highestPrice = document.getElementById("search-price-highest").value;
+
+  let carlist = await retrieveCarsFromDatabase();
+  // RADI ALI NE BI BILO LOSE UBACITI I SORTIRANJE PO CENI ASC I DESC...
+
+  if (highestPrice === "") {
+    highestPrice = 99999999999;
+  }
+
+  if (lowestPrice === "") {
+    lowestPrice = 0;
+  }
+
+  let filteredByPrice = carlist.filter((car) => {
+    if (car.price) {
+      //zrelo za izbacivanje kada napravim da ne moze da se doda auto bez cene
+      if (
+        car.price >= parseInt(lowestPrice) &&
+        car.price <= parseInt(highestPrice)
+      ) {
+        return car;
+      }
     }
-
-
-    let filteredByPrice = carlist.filter( car => {
-        if(car.price){  //zrelo za izbacivanje kada napravim da ne moze da se doda auto bez cene
-            if(car.price >= parseInt(lowestPrice) && car.price <= parseInt(highestPrice)){
-                return car;
-            }
-        }
-            
-    });
-    renderElements(filteredByPrice);
+  });
+  renderElements(filteredByPrice);
 }
 
 // NAPRAVITI DA OBA SEARCHA VAZE U ISTO VREME AKO JE TO MOGUCE.....
